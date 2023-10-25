1dc93c78b1268f66d31b617647ef2dead8fcda73
JuanSaHecht <juanhecht23@gmail.com>
2023-10-25 17:11:17 +0200

    ::

--- Ud2/Practica/ajedrez_web/chessView.php
+++ Ud2/Practica/ajedrez_web/chessView.php
@@ -70,123 +70,72 @@
 
     ini_set('display_errors', 'On');
     ini_set('html_errors', 0);
+    
+        function deadPiecesWhite($board) // recieves a string with state of board
+        {
+            $alivePieces = countPieces($board);
 
+            $maxPiecesAlive = array(
+                "ROWH" => 2,
+                "KNWH" => 2,
+                "BIWH" => 2,
+                "QUWH" => 1,
+                "KIWH" => 1,
+                "PAWH" => 8,
+            );
 
-    // function CreateEmptyBoard()
-    // {
-    //     $rows = 8;
-    //     $columns = 8;
-    
-    //     $board = array();
-    //     $position = 0;
-    
-    //     $text = "";
-    
-    //     for ($row = $rows - 1; $row >= 0; $row--) {
-    
-    //         for ($column = 0; $column < $columns; $column++) {
-    //             if ((($row + $column) % 2) != 0) {
-    //                 $text += "0000";
-    
-    //             } else {
-    //                 $text += "####,";
-    //             }
-    
-    //         }
-    //     }
-    
-    //     return $text;
-    // }
-    
-    // function ArrayToString($array)
-    // {
-    //     $string = "";
-    
-    //     for( $i = 0; $i < count($array); $i++ )
-    //     {
-    //        for ($y=0; $y < count($array[0]); $y++) { 
-    //         $string += $array[$i][$y];
-    //        } 
-    //     }
-    
-    //     return $string;
-    // }
-    
-    // function tableroInicio()
-    // {
-    //     // CREAR TABLERO$initialPiecesWhite =
-    //     $rows = 8;
-    //     $columns = 8;
-    
-    //     $tablero = array([$rows], [$columns]);
-    
+            $deadPieces = array();
 
-    //         for ($row = $columns - 1; $row >= 0; $row--) 
-    //         {
-    
-    //             if ($row != 0 && $row != 1 && $row != 6 && $row != 7) 
-    //             {
-    //                 for ($column = 0; $column < $columns; $column++) 
-    //                 {
-    //                     if ((($row + $column) % 2) != 0) {
-    //                         $tablero[$row][$column] = "0000";
-    
-    //                     } else {
-    //                         $tablero[$row][$column] = "####";
-    //                     }
-    //                 }  
-    //             } 
-    //             elseif ($row == 0) 
-    //             {
-    //                 $tablero[$row]="ROBL", "KNBL", "BIBL", "QUBL", "KIBL", "BIBL", "KNBL", "ROBL";
-    //             } 
-    //             elseif ($row == 1) 
-    //             {
-    //                     for ($column = 0; $column < $columns; $column++) {
-    //                         $tablero[$row][$column] = "PABL";
-    //                     }
-    //             } 
-    //             elseif ($row == 6) 
-    //             {
-    //                     for ($column = 0; $column < $columns; $column++) {
-    //                         $tablero[$row][$column] = "PAWH";
-    //                     }
-    //             } 
-    //             elseif ($row == 7) 
-    //                 {
-    //                     array("ROWH", "KNWH", "BIWH", "QUWH", "KIWH", "BIWH", "KNWH", "ROWH");
-    //                 }
-    //         }
-    //     }
-    
+            foreach ($alivePieces as $key => $value) {
+                if (array_key_exists($key,$maxPiecesAlive) && substr($key,2) == "WH") {
+                    $diference = $maxPiecesAlive[$key] - $value;
+                    if ($diference != 0) {
+                        $deadPieces[$key] = $diference;
+                    }
+                }
+            }
+
+            return $deadPieces;
+        }
+
+        function deadPiecesBlack($board) // recieves a string with state of board
+        {
+            $alivePieces = countPieces($board);
+
+            $maxPiecesAlive = array(
+                "ROBL" => 2,
+                "KNBL" => 2,
+                "BIBL" => 2,
+                "QUBL" => 1,
+                "KIBL" => 1,
+                "PABL" => 8,
+            );
+
+            $deadPieces = array();
+
+            foreach ($alivePieces as $key => $value) {
+                if (array_key_exists($key,$maxPiecesAlive) && substr($key,2) == "WH") {
+                    $diference = $maxPiecesAlive[$key] - $value;
+                    if ($diference != 0) {
+                        $deadPieces[$key] = $diference;
+                    }
+                }
+            }
+
+            return $deadPieces;
+        }
 
 
     function countPieces($board) // recieves a string
-    {
-        // $whitePawns = 8;
-        // $whiteRooks = 2;
-        // $whiteKnights = 2;
-        // $whiteBishops = 2;
-        // $whiteQueen = 1;
-        // $whiteKing = 1;
-
-        // $blackPawns = 8;
-        // $blackRooks = 2;
-        // $blackKnights = 2;
-        // $blackBishops = 2;
-        // $blackQueen = 1;
-        // $blackKing = 1;
-
+    {  
         $text = explode(",", $board);
         $piecesAlive = array();
 
         for ($i = 0; $i < count($text); $i++) {
 
-            if(array_key_exists($text[$i],$piecesAlive))
-            {
+            if (array_key_exists($text[$i], $piecesAlive)) {
                 $piecesAlive[$text[$i]]++;
-            }else 
-            {
+            } else {
                 $piecesAlive[$text[$i]] = 1;
             }
         }
@@ -195,30 +144,9 @@
     }
 
 
-    function DrawChessGame($board) //Recieves a string
+    function deadTableWhite($board) // recieves a string with state of board
     {
-        $initialPieces = array("ROBL", "KNBL", "BIBL", "QUBL", "KIBL", "PABL", "ROBL", "KNBL", "BIBL", "ROWH", "KNWH", "BIWH", "QUWH", "KIWH", "ROWH", "KNWH", "BIWH", "PAWH");
-
-
-
-
-        // Converts string into an array and then the array into a matrix
-        $text = explode(",", $board);
-        $position = 0;
-        for ($i = 0; $i < 8; $i++) {
-            for ($y = 0; $y < 8; $y++) {
-                $boardArray[$i][$y] = $text[$position];
-                $position++;
-            }
-        }
-
-
-
-
-
-
-
-
+        $deadPieces = deadPiecesWhite($board);
         // show dead white pieces table 
         echo "<table \"dead\">";
         for ($i = 0; $i < 2; $i++) {
@@ -227,6 +155,11 @@
 
             for ($y = 0; $y < 8; $y++) {
                 echo "<td class=\"dead\">";
+
+                foreach ($deadPieces as $key => $value) {
+                    echo "<img src=\"Icons/" . $key . ".png\" >";
+                }
+
                 echo "</td>";
             }
 
@@ -234,10 +167,30 @@
 
         }
         echo "</table>";
+    }
 
+    function deadTableBlack($board) // recieves a string with state of board
+    {
+        // show dead white pieces table 
+        echo "<table \"dead\">";
+        for ($i = 0; $i < 2; $i++) {
 
+            echo "<tr>";
 
+            for ($y = 0; $y < 8; $y++) {
+                echo "<td class=\"dead\">";
+                deadPiecesBlack($board);
+                echo "</td>";
+            }
 
+            echo "</tr>";
+
+        }
+        echo "</table>";
+    }
+
+    function inGameBoard($boardArray) // recieves a matrix with board state
+    {
         // show board
         echo "<table class=\"board\"";
 
@@ -271,36 +224,39 @@
 
         }
         echo "</table>";
-
-
-
-        // show dead black pieces table
-        echo "<table \"dead\">";
-        $shownPieces = 0;
-        for ($i = 0; $i < 2; $i++) {
-
-            echo "<tr>";
-
-            for ($y = 0; $y < 8; $y++) {
-                echo "<td class=\"dead\">";
-
-                echo "</td>";
-            }
-
-            echo "</tr>";
-
-        }
-        echo "</table>";
     }
 
 
+    function DrawChessGame($board) //Recieves a string
+    {
+        // $initialPieces = array("ROBL", "KNBL", "BIBL", "QUBL", "KIBL", "PABL", "ROBL", "KNBL", "BIBL", "ROWH", "KNWH", "BIWH", "QUWH", "KIWH", "ROWH", "KNWH", "BIWH", "PAWH");
+    
+        // Converts string into an array and then the array into a matrix
+        $text = explode(",", $board);
+        $position = 0;
+        for ($i = 0; $i < 8; $i++) {
+            for ($y = 0; $y < 8; $y++) {
+                $boardArray[$i][$y] = $text[$position];
+                $position++;
+            }
+        }
+
+        deadTableWhite($board);
+
+        inGameBoard($boardArray);
+
+        deadTableBlack($board);
+    }
+    $board = "ROWH,KNWH,BIWH,QUWH,KIWH,BIWH,KNWH,ROWH,PAWH,PAWH,PAWH,PAWH,PAWH,PAWH,0000,PAWH,0000,####,0000,####,0000,####,0000,####,####,0000,####,0000,####,0000,####,0000,0000,####,0000,####,0000,####,0000,####,0000,####,0000,####,0000,####,0000,####,PABL,PABL,PABL,PABL,PABL,PABL,PABL,PABL,ROBL,KNBL,BIBL,QUBL,KIBL,BIBL,KNBL,ROBL";
+
     echo "<div class=\"board\">";
-    DrawChessGame("ROBL,KNBL,BIBL,QUBL,KIBL,BIBL,KNBL,ROBL,PABL,PABL,PABL,PABL,PABL,PABL,PABL,PABL,0000,####,0000,####,0000,####,0000,####,####,0000,####,0000,####,0000,####,0000,0000,####,0000,####,0000,####,0000,####,0000,####,0000,####,0000,####,0000,####,PAWH,PAWH,PAWH,PAWH,PAWH,PAWH,PAWH,PAWH,ROWH,KNWH,BIWH,QUWH,KIWH,BIWH,KNWH,ROWH");
+    DrawChessGame($board);
     echo "</div>";
 
 
-    var_dump(countPieces("ROBL,KNBL,BIBL,QUBL,KIBL,BIBL,KNBL,ROBL,PABL,PABL,PABL,PABL,PABL,PABL,PABL,PABL,0000,####,0000,####,0000,####,0000,####,####,0000,####,0000,####,0000,####,0000,0000,####,0000,####,0000,####,0000,####,0000,####,0000,####,0000,####,0000,####,PAWH,PAWH,PAWH,PAWH,PAWH,PAWH,PAWH,PAWH,ROWH,KNWH,BIWH,QUWH,KIWH,BIWH,KNWH,ROWH"));
-
+    var_dump(deadPiecesWhite($board));
+    echo "<br>";
+    var_dump(deadPiecesBlack($board));
     ?>
 </body>
 
