9e1cc2ec2ce77c5b1227c99ea210c363be352ac7
JuanSaHecht <juanhecht23@gmail.com>
2024-02-05 20:21:20 +0100

    dd

--- Ud4/Practica/chess_web_php/chess_game.sql
+++ Ud4/Practica/chess_web_php/chess_game.sql
@@ -1,32 +1,37 @@
-CREATE SCHEMA chess_game;
+CREATE DATABASE `chess_game` ;
 
-USE chess_game;
+CREATE TABLE `T_Players` (
+  `ID` int NOT NULL AUTO_INCREMENT,
+  `name` varchar(30) NOT NULL,
+  `email` varchar(50) NOT NULL,
+  `profileType` enum('silver','gold') DEFAULT NULL,
+  `password` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`ID`),
+  UNIQUE KEY `email` (`email`)
+) ENGINE=InnoDB AUTO_INCREMENT=8 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
-CREATE TABLE T_Players(
-ID int primary key auto_increment,
-name varchar(30) not null,
-email varchar(50) UNIQUE,
-password varchar(30) not null
-);
+CREATE TABLE `T_Matches` (
+  `ID` int NOT NULL AUTO_INCREMENT,
+  `title` varchar(50) NOT NULL,
+  `white` int NOT NULL,
+  `black` int NOT NULL,
+  `startDate` datetime NOT NULL,
+  `endDate` datetime DEFAULT NULL,
+  `winner` varchar(10) DEFAULT NULL,
+  `state` varchar(20) NOT NULL DEFAULT (_utf8mb4'En curso'),
+  PRIMARY KEY (`ID`),
+  KEY `white` (`white`),
+  KEY `black` (`black`),
+  CONSTRAINT `T_Matches_ibfk_1` FOREIGN KEY (`white`) REFERENCES `T_Players` (`ID`),
+  CONSTRAINT `T_Matches_ibfk_2` FOREIGN KEY (`black`) REFERENCES `T_Players` (`ID`)
+) ENGINE=InnoDB AUTO_INCREMENT=420 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
-CREATE TABLE T_Matches(
-ID int primary key auto_increment,
-title varchar(50) not null,
-white int not null,
-black int not null,
-startDate datetime not null,
-endDate datetime,
-winner varchar(10),
-state varchar(20) not null default("En curso"),
-    FOREIGN KEY (white) REFERENCES T_Players(ID),
-    FOREIGN KEY (black) REFERENCES T_Players(ID)
-);
-
-CREATE TABLE T_Board_Status(
-ID int auto_increment, 
-IDGame int,
-board varchar(322), /* Modificar si es necesario */
-primary key(ID,IDGame),
-FOREIGN KEY (IDGame) REFERENCES T_Matches(ID)
-);
+CREATE TABLE `T_Board_Status` (
+  `ID` int NOT NULL AUTO_INCREMENT,
+  `IDGame` int NOT NULL,
+  `board` varchar(322) DEFAULT NULL,
+  PRIMARY KEY (`ID`,`IDGame`),
+  KEY `IDGame` (`IDGame`),
+  CONSTRAINT `T_Board_Status_ibfk_1` FOREIGN KEY (`IDGame`) REFERENCES `T_Matches` (`ID`)
+) ENGINE=InnoDB AUTO_INCREMENT=37 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
